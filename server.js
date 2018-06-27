a1025bc8367441ef2666008e4db8347578722049
rfarney3 <farneyr@yahoo.com>
2018-06-26 17:51:42 -0700

    Created User Schema and User Registration

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users.js");
 const profile = require("./routes/api/profile.js");
 const posts = require("./routes/api/posts.js");
 
 const app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Db Config
 const db = require("./config/keys.js").mongoURI;
