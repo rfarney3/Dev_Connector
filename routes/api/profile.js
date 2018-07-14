404b601d05a78173bb4607c9d4b4445a1119b7f1
rfarney3 <farneyr@yahoo.com>
2018-07-13 18:00:47 -0700

    Profile field validations

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+//Load Validation
+const validateProfileInput = require("../../validation/profile");
+
 //Load Profile Model
 const Profile = require("../../models/Profile");
 //Load User Profile
@@ -29,6 +32,7 @@ router.get(
     const errors = {};
 
     Profile.findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"])
       .then(profile => {
         if (!profile) {
           errors.noprofile = "There is no profile for this user";
@@ -41,19 +45,27 @@ router.get(
 );
 
 // @route   POST api/profile
-// @desc    Create user profile
+// @desc    Create or Update user profile
 // @access  Private
 
 router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    //Check Validations
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
     //Get fields
     const profileFields = {};
     profileFields.user = req.user.id;
     if (req.body.handle) profileFields.handle = req.body.handle;
     if (req.body.company) profileFields.company = req.body.company;
     if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.website) profileFields.website = req.body.website;
     if (req.body.bio) profileFields.bio = req.body.bio;
     if (req.body.status) profileFields.status = req.body.status;
     if (req.body.githubusername) profileFields.githubusername = req.body.handle;
