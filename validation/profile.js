a2ead1dbe3a5fb0277cc9e1ee9ddcaf962057242
rfarney3 <farneyr@yahoo.com>
2018-07-22 16:17:04 -0700

    Delete Education and Experience Routes

--- validation/profile.js
+++ validation/profile.js
@@ -8,7 +8,7 @@ module.exports = function validateProfileInput(data) {
   data.status = !isEmpty(data.status) ? data.status : "";
   data.skills = !isEmpty(data.skills) ? data.skills : "";
 
-  if (!Validator.isLength(data.handle, { min: 6, max: 40 })) {
+  if (!Validator.isLength(data.handle, { min: 2, max: 40 })) {
     errors.handle = "Handle must be between 2 and 40 characters";
   }
 
